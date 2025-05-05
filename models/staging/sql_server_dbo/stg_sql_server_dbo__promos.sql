f56cd01500a385db71d26e3c08b14a83d1c6660e
Jonás Galindo <jonasgsx@hotmail.com>
2025-05-05 18:01:48 +0000

    tests on sources and wip stgs

--- models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
@@ -1,7 +1,21 @@
--- with base as (
---   select * from {{ ref('stg_sql_server_dbo__promos') }}
--- )
+SELECT
+  {{ dbt_utils.generate_surrogate_key(['promo_id']) }} as promo_id,
+  promo_id as promo_description,
+  discount,
+  CASE
+    WHEN lower(status) = 'active' THEN 1
+    WHEN lower(status) = 'inactive' THEN 0
+    ELSE NULL
+  END as status
+--   convert_timezone('UTC', _fivetran_synced) as _fivetran_synced
+FROM {{ source('sql_server_dbo', 'promos') }}
+WHERE _fivetran_deleted IS NULL
 
--- select
---   base.*
--- from base
+UNION ALL
+
+SELECT
+  {{ dbt_utils.generate_surrogate_key(["'sin-promo'"]) }} as promo_id,
+  'sin-promo' as promo_description,
+  0 as discount,
+  1 as status
+--   convert_timezone('UTC', _fivetran_synced) as _fivetran_synced
